4b69f8a2675535a87fde07adbf3f60e271756bbc
Osvaldo Figueroa <figaustin@yahoo.com>
2022-12-02 02:28:24 -0500

    code cleanup

--- src/main/java/com/etsuni/harderspawns/HarderSpawns.java
+++ src/main/java/com/etsuni/harderspawns/HarderSpawns.java
@@ -1,12 +1,8 @@
 package com.etsuni.harderspawns;
 
-import jdk.jfr.EventType;
-import jdk.management.jfr.EventTypeInfo;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.event.Event;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -28,20 +24,13 @@ public final class HarderSpawns extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
-    public File getCustomConfigFile() {
-        return this.customConfigFile;
-    }
 
     public FileConfiguration getCustomConfig() {
         return this.customConfig;
     }
 
-    public void setCustomConfig(FileConfiguration fileConfiguration) {
-        this.customConfig = fileConfiguration;
-    }
 
     private void createCustomConfig() {
         customConfigFile = new File(getDataFolder(), "config.yml");
